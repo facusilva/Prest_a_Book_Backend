15337b353f5217c815fce22f978bb2b33fc678c1
AlbertoJiLo <alberto_tgn_14@hotmail.com>
2022-11-17 16:38:04 +0100

    Organizacion ramas

--- src/main/java/com/example/prestabook/dto/Gender.java
+++
@@ -1,5 +0,0 @@
-package com.example.prestabook.dto;
-
-public enum Gender {
-		H,M,Otro
-}
--- src/main/java/com/example/prestabook/dto/Genre.java
+++
@@ -1,5 +0,0 @@
-package com.example.prestabook.dto;
-
-public enum Genre {
-		Terror,Fantasia,Thriller,Drama,Comedia,Romance,Ensayo,Autoayuda
-}
