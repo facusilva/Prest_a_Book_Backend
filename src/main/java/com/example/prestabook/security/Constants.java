c3925458e41bf83f3e759bf53a35bec544c838be
facusilva <55622040+facusilva@users.noreply.github.com>
2022-12-02 18:56:24 +0100

    Update Constants.java

--- src/main/java/com/example/prestabook/security/Constants.java
+++ src/main/java/com/example/prestabook/security/Constants.java
@@ -4,14 +4,14 @@ public class Constants {
 
 	// Spring Security
 
-	public static final String LOGIN_URL = "/login";
-	public static final String HEADER_AUTHORIZACION_KEY = "Authorization";
-	public static final String TOKEN_BEARER_PREFIX = "Bearer ";
+	public static final String LOGIN_URL = "${LOGIN_URL}";
+	public static final String HEADER_AUTHORIZACION_KEY = "${HEADER_AUTHORIZACION_KEY}";
+	public static final String TOKEN_BEARER_PREFIX = "${TOKEN_BEARER_PREFIX} ";
 
 	// JWT
 
-	public static final String ISSUER_INFO = "Jose Marín";
-	public static final String SUPER_SECRET_KEY = "1234";
+	public static final String ISSUER_INFO = "${ISSUER_INFO}";
+	public static final String SUPER_SECRET_KEY = "${SUPER_SECRET_KEY}";
 	public static final long TOKEN_EXPIRATION_TIME = 864_000_000; // 10 day
 
 }
